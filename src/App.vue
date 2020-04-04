9c3e4efe480f6496ff1d3d9892d6de44fc8186d9
Ariane Holford <aribomop@gmail.com>
2020-04-04 10:57:49 +0100

    fetch api data

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>SpaceX App</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "app",
+  data(){
+    return {
+      launches: [],
+    };
+  },
+  methods: {
+    getLaunches: function(){
+      fetch("https://api.spacexdata.com/v3/launches")
+      .then(res => res.json())
+      .then(launches => this.launches = launches)
+    }
+  },
+  mounted() {
+    this.getLaunches();
   }
-}
+
+
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+h1{
+  color:salmon;
 }
 </style>
