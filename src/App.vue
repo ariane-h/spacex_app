c77e7a67df42af6decae4b3c40640bf747e78a22
Ariane Holford <aribomop@gmail.com>
2020-04-04 13:22:09 +0100

    emit search query input

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,26 @@
 <template>
   <div id="app">
     <h1>SpaceX App</h1>
+    <launch-search></launch-search>
     <launch-list :launches="launches"></launch-list>
   </div>
 </template>
 
 <script>
 import LaunchList from './components/LaunchList.vue';
+import LaunchSearch from './components/LaunchSearch.vue';
 
 
 export default {
   name: "app",
   components: {
     "launch-list": LaunchList,
+    "launch-search": LaunchSearch
   },
   data(){
     return {
       launches: [],
+      searchQuery: ""
     };
   },
   methods: {
---
+++ src/components/LaunchSearch.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+    <div>
+        <h2> search the launches: </h2>
+        <input type="text" v-model="query" @input="sendQuery(query)" placeholder="search launches"></input>
+    </div>
+</template>
+
+<script>
+
+import { eventBus } from "@/main.js";
+
+export default {
+    name: "launch-search",
+    data(){
+        return{
+            query: ""
+        };
+    },
+    methods: {
+        sendQuery: function(query) {
+        eventBus.$emit("query-input", query);
+        },
+    },
+}
+</script>
+
+<style>
+
+</style>
