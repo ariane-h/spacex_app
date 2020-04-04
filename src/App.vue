92a801117be0ab140342a25467d041f6b3efd3d8
Ariane Holford <aribomop@gmail.com>
2020-04-04 13:42:19 +0100

    receive search query input

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="app">
     <h1>SpaceX App</h1>
-    <launch-search></launch-search>
+    <launch-search :searchQuery="searchQuery"></launch-search>
     <launch-list :launches="launches"></launch-list>
   </div>
 </template>
 
 <script>
+import { eventBus } from "@/main.js";
 import LaunchList from './components/LaunchList.vue';
 import LaunchSearch from './components/LaunchSearch.vue';
 
@@ -17,21 +18,26 @@ export default {
     "launch-list": LaunchList,
     "launch-search": LaunchSearch
   },
+
   data(){
     return {
       launches: [],
       searchQuery: ""
     };
   },
+
   methods: {
     getLaunches: function(){
       fetch("https://api.spacexdata.com/v3/launches")
       .then(res => res.json())
       .then(launches => this.launches = launches)
     }
+
   },
   mounted() {
     this.getLaunches();
+
+    eventBus.$on("query-input", query => (this.searchQuery = query));
   }
 
 
